8d4ae9103ae7998b9cab4fb25710feb8032f383f
balasri94 <129168327+balasri94@users.noreply.github.com>
2023-09-04 16:09:23 +0530

    Update conf_f.c

--- Fault_Mng/conf_f.c
+++ Fault_Mng/conf_f.c
@@ -1,6 +1,16 @@
+uint8_t sens_data;
+
 uint8_t sens_IP_read(void)
 {
 	uint8_t decimal;
 	decimal=var.pin_1<<1|var.pin_2;
 	return decimal;
+}
+
+uint8_t sens_OP_read()
+{
+	if(sens_data)
+		return 1;
+	else
+		return 0;
 }
