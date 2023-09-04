d5d1667ca058d7ca568712974a0a6da579b81209
balasri94 <129168327+balasri94@users.noreply.github.com>
2023-09-04 15:59:26 +0530

    Update Fault_mng.c

--- Fault_Mng/Fault_mng.c
+++ Fault_Mng/Fault_mng.c
@@ -3,12 +3,6 @@
 sens_pin_2_1_check var;
 uint8_t fault_cases;
 
-uint8_t sens_IP_read(void)
-{
-	uint8_t decimal;
-	decimal=var.pin_1<<1|var.pin_2;
-	return decimal;
-}
 
 void sens_fault_check()
 {
