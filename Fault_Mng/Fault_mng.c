705379bf20a89a0643321fbca93f8235786c95bd
balasri94 <129168327+balasri94@users.noreply.github.com>
2023-09-04 17:12:53 +0530

    Update Fault_mng.c

--- Fault_Mng/Fault_mng.c
+++ Fault_Mng/Fault_mng.c
@@ -14,14 +14,14 @@ void sens_fault_check()
 	
 	switch(fault_cases)
 	{
-		case SNA_Both_gnd :
-		case SNV_Both_gnd :
-		case SNA_Revs_conn :
-		case SNV_Revs_conn :
-		case SNA_Prop_conn :
-		case No_fault :
-		case SNA_Both_high :
-		case SNV_Both_high :
+		case SNA_Both_gnd :	fault_count++; write_to_EEPROM(FAULT_ID_SNA_Both_gnd); warning_lamp( R_led,  Blink_1); break;  // SNA == SIGNAL NOT AVAILABLE
+		case SNV_Both_gnd :	fault_count++; write_to_EEPROM(FAULT_ID_SNV_Both_gnd); break;					// SNV == SIGNAL NOT VALID 
+		case SNA_Revs_conn :	fault_count++; write_to_EEPROM(FAULT_ID_SNA_Revs_conn); warning_lamp( R_led,  Blink_7); break;
+		case SNV_Revs_conn :	fault_count++; write_to_EEPROM(FAULT_ID_SNV_Revs_conn); break;
+		case SNA_Prop_conn :	fault_count++; write_to_EEPROM(FAULT_ID_SNA_Prop_conn); warning_lamp( R_led,  Blink_1000); break;
+		case No_fault :		 break;
+		case SNA_Both_high :	fault_count++; write_to_EEPROM(FAULT_ID_SNA_Both_high); warning_lamp( R_led,  Blink_20); break;
+		case SNV_Both_high :	fault_count++; write_to_EEPROM(FAULT_ID_SNV_Both_high); break;
 		default :
 	}
 
