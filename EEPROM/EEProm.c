48e8dc500242c179d5f1baf3bcdf13b3f681be68
lokabharathreddy <127173658+lokabharathreddy@users.noreply.github.com>
2023-09-09 15:17:17 +0530

    Update EEProm.c

--- EEPROM/EEProm.c
+++ EEPROM/EEProm.c
@@ -1,19 +1,23 @@
 #include "EEProm.h"
 
-bool write_to_EE(char sens_fault_ID)
+ uint32_t write_to_EE(char sens_fault_ID)
 {
     EEDATA = sens_fault_ID;
     // v = HAL_i2c_eeprom_write(eeprom_addr,mem_addr,sens_fault_ID);
     if(ack==1)
-	    return true;
+	    return 1;
     else
-	    return false;
+	    return FAULT_ID_EEPROM_WRITE;
 }
 uint32_t read_from_EE(void)
 {
 		//EEADR = address;
     uint16_t EEDATA;
 		EEDATA	= hal_i2c_eeprom_read(address);
+	 if(ack==1)
+	    return EEDATA;
+         else
+	    return FAULT_ID_EEPROM_READ;
 	
     return EEDATA;
 }
