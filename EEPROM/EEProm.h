248ba58fa6f063e2e5fac7270cfc7a1b6c1ca977
lokabharathreddy <127173658+lokabharathreddy@users.noreply.github.com>
2023-09-09 15:15:23 +0530

    Update EEProm.h

--- EEPROM/EEProm.h
+++ EEPROM/EEProm.h
@@ -1,6 +1,9 @@
 #define EEPROM_WRITE 0xA1
 #define EEPROM_READ 0xA0
 #define EEPROM_MEM_LOC 0x00
+#define  FAULT_ID_EEPROM_WRITE	 0XEEAA0001
+#define  FAULT_ID_EEPROM_READ	 0XEEAA0000
+
 
 void write_to_EE (char);
 char read_from_EE (char);
